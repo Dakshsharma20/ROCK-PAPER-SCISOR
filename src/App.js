c0b9031369a38e40489f723830163d06d89d1f8b
dakshsharma20 <ukdk131@gmail.com>
2024-08-23 13:35:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const[userchoice, setuserchoice] = useState("")
+  const[computerchoice, setcomputerchoice] = useState("")
+  const[winner, setwinner] = useState("")
+  
+
+  const choicelist = [`ROCK`, `PAPER`, `SCISOR`]
+
+  const startgame = (user)=>{
+    setuserchoice(user)
+
+    const randomNum = Math.floor(Math.random()*3)
+    const localCompChoice = choicelist[randomNum]
+    setcomputerchoice(localCompChoice)
+
+    if(user==localCompChoice){
+      setwinner("DRAW")
+    }
+    else if((user == "ROCK" && localCompChoice == "SCISOR") || (user == "PAPER" && localCompChoice == "ROCK") || (user == "SCISOR" && localCompChoice == "PAPER")){
+      setwinner("USER")
+    }
+    else{
+      setwinner("COMPUTER")
+    }
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col justify-center items-center'>
+      <div>CHOOSE :</div>
+      <div className='flex gap-4'>
+        <div onClick={()=>startgame("ROCK")}>ROCK</div>
+        <div onClick={()=>startgame("PAPER")}>PAPER</div>
+        <div onClick={()=>startgame("SCISOR")}>SCISOR</div>
+      </div>
+      <div>USER :{userchoice}</div>
+      <div>CPMPUTER :{computerchoice}</div>
+      <div>USER :{winner}</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
